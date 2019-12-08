0277cd39b6bda2bb24ae73887fac358839392793
Frank <572220216@qq.com>
2019-12-08 11:46:31 +0100

    fix2.0 拉取下来先运行 npm install .

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -8,7 +8,7 @@ import {
 import './index.css';
 import App from './App';
 import Heatmap from './components/Heatmap';
-import candlestick-example from './components/candlestick-example';
+import candlestick from './components/ShowCandlestick';
 // 这里是将candlestick 引入进来，不然无法识别这个组件
 import ShowForceGraph from './components/ShowForceGraph';
 import Scannerplot from './components/Scannerplot';
@@ -35,7 +35,7 @@ export default class Hello extends Component{
             　　　　　　<li><Link to="/ShowForceGraph" className="btn btn-outline-success btn-sm">ForceDirectedGraph</Link></li>
             　　　　　　<li><Link to="/Heatmap" className="btn btn-outline-success btn-sm">Heatmap</Link></li>
             　　　　　　<li><Link to="/Scannerplot" className="btn btn-outline-success btn-sm">Scannerplot</Link></li>
-            　　　　　　<li><Link to="/candlestick-example" className="btn btn-outline-success btn-sm">candlestick-example</Link></li>
+            　　　　　　<li><Link to="/candlestick" className="btn btn-outline-success btn-sm">candlestick-example</Link></li>
         　　 　　　　</ul>
                     <hr />
                     <div className="con">
@@ -43,7 +43,7 @@ export default class Hello extends Component{
                     <Route path="/ShowForceGraph" component={ShowForceGraph} />
                     <Route path="/Heatmap" component={Heatmap} />
                     <Route path="/Scannerplot" component={Scannerplot} />
-                    <Route path="/candlestick-example" component={candlestick-example} />
+                    <Route path="/candlestick" component={candlestick} />
               
                     </div>
                     
